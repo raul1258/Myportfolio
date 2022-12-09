27a134a3c63068653d924146cb9d41521e67ed43
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-12-09 21:32:05 +0530

    skills update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AboutMe from "./components/AboutMe";
 import Header from "./components/header";
 import Intro from "./components/intro";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
       <div className="container">
         <Intro/>
         <AboutMe/>
-        
+        <Projects/>
+        <Skills />
       </div>
-      <Projects/>
+      
     </div>
   );
 }
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import "./style.css"
+
+function Skills() {
+  return (
+    <section className='skills section' id='skills'>
+        <h2 className='section_title'>Skills</h2>
+        <span className='section_subtitle'>My technical level</span>
+        <div className='skills_container container grid'>
+            <div className='skills_content'>
+                <h3 className='skills_title'>Frontend Developer</h3>
+                <div className='skills_box'>
+                    <div className='skills_group'>
+                        <div className='skills_data'>
+                            <div>
+                                <h3 className='skills_name'>HTML</h3>
+                                <span className='skills_level'>Basic</span>
+                            </div>
+                        </div>
+                        <div className='skills_data'>
+                            <div>
+                                <h3 className='skills_name'>CSS</h3>
+                                <span className='skills_level'>Advanced</span>
+                            </div>
+                        </div>
+                        <div className='skills_data'>
+                            <div>
+                                <h3 className='skills_name'>JavaScript</h3>
+                                <span className='skills_level'>Basic</span>
+                            </div>
+                        </div>
+                        <div className='skills_data'>
+                            <div>
+                                <h3 className='skills_name'>React Js</h3>
+                                <span className='skills_level'>Intermediate</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Skills
