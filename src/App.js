ec63e9263bf97d30116024af85c7a77c913d22f2
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-12-16 22:00:46 +0530

    education update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Intro/>
         <AboutMe/>
         <Skills />
-        <Qualification/>
+        <Qualification />
         <Projects/>
       </div>
       
---
+++ src/components/qualification/Qualification.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Qualification() {
+  return (
+   <div className="qualification section">
+        <h2 className='section_title'>Qualification</h2>
+        <span className='section_subtitle'>My Personal Journey</span>
+
+        <div className='qualification_container container'>
+            <div className='qualification_tabs'>
+                <div className='qualification_button button_flex'>
+                  <h1>class 12</h1>
+                  <h1>class 12</h1>
+                  <h1>class 12</h1>
+                  <h1>class 12</h1>
+                </div>
+            </div>
+
+        </div>
+   </div>
+  )
+}
+
+export default Qualification
